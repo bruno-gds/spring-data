17573d86368b62f8e0a9b98dc53298cd0f815969
bruno-gds <brunog.damascena@gmail.com>
2024-02-29 21:30:50 -0300

    transformando Item em entity

--- pedidos/src/main/java/br/com/organicxpto/pedidos/Item.java
+++ pedidos/src/main/java/br/com/organicxpto/pedidos/Item.java
@@ -1,9 +1,17 @@
 package br.com.organicxpto.pedidos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class Item {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long idProduct;
 
 	private BigDecimal quantidade;
